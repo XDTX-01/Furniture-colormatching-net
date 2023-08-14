d10bb7c6d1af1a2df362bfa7e3e7197063002882
XIAODENG <112189072+xdtx-0111@users.noreply.github.com>
2023-08-14 17:27:56 +0800

    小修改

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -468,6 +468,11 @@ export default {
             img: "./img/布/A100米格布.jpg",
             name: "布",
           },
+          {
+            text: "20X799-10棕色条纹布",
+            img: "./img/布/20X799-10棕色条纹布.jpg",
+            name: "布",
+          },
         ],
         窗帘: [
           {
