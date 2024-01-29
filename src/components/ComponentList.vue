e9328eeee2fb311a282b8ac8e9d5252171b3dc73
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2024-01-29 16:09:17 +0800

    修改

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -488,6 +488,11 @@ export default {
             img: "./img/布/CL-21028-01米白布.jpg",
             name: "布",
           },
+            {
+            text: "D807-01-1白色",
+            img: "./img/布/D807-01-1白色.jpg",
+            name: "布",
+          },
           {
             text: "DS04967-1A米白布",
             img: "./img/布/DS04967-1A米白布.jpg",
@@ -510,7 +515,7 @@ export default {
                 name: "布",
             },
             {
-                text: "R8006-20-1橙黄",
+                text: "R806-20-1橙黄",
                 img: "./img/布/R8006-20-1橙黄.jpg",
                 name: "布",
             },
