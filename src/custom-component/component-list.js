e66ae4c6396a9816b7c8a67998f0cce56a663e24
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2024-05-28 09:05:04 +0800

    修改

--- src/custom-component/component-list.js
+++ src/custom-component/component-list.js
@@ -791,7 +791,16 @@ let newList = [
         img: "./img/其它/幻影乳胶漆.jpg",
         name: "乳胶漆",
     },
-
+    {
+        text: "白色乳胶漆",
+        img: "./img/其它/白色乳胶漆.jpg",
+        name: "乳胶漆",
+    },
+    {
+        text: "幻影色乳胶漆",
+        img: "./img/其它/幻影色乳胶漆.jpg",
+        name: "乳胶漆",
+    },
     {
         text: "灰铝",
         img: "./img/其它/灰铝.jpg",
