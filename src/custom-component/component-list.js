b8e7f78f4b4002992dec125326aa0c9b1cbf29b9
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2024-06-17 10:50:39 +0800

    Update component-list.js

--- src/custom-component/component-list.js
+++ src/custom-component/component-list.js
@@ -559,6 +559,12 @@ let newList = [
         name: "窗帘",
     },
     {
+        text: "BW01三色条纹",
+        img: "./img/墙布挂板/BW01三色条纹.jpg",
+        flag: false,
+        name: "墙布挂板",
+    },
+    {
         text: "BW05白色草纹",
         img: "./img/墙布挂板/BW05白色草纹.jpg",
         flag: false,
