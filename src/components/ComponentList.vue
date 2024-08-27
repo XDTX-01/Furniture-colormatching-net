ee1f47748d062d0f092989dccc3e3072145c13e2
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2024-08-27 11:29:20 +0800

    Update ComponentList.vue

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -637,6 +637,36 @@ export default {
             img: "./img/窗帘/SPC-AZ-23浅蓝.jpg",
             name: "窗帘",
           },
+             {
+        text: "SPC-AK-09S灰色条纹",
+        img: "./img/窗帘/SPC-AK-09S灰色条纹.jpg",
+        name: "窗帘",
+    }, 
+ {
+        text: "SPC-AK-18S白色",
+        img: "./img/窗帘/SPC-AK-18S白色.jpg",
+        name: "窗帘",
+    }, 
+ {
+        text: "SPC-AK-19S米色",
+        img: "./img/窗帘/SPC-AK-19S米色.jpg",
+        name: "窗帘",
+    }, 
+ {
+        text: "SPC-AK-20S灰色",
+        img: "./img/窗帘/SPC-AK-20S灰色.jpg",
+        name: "窗帘",
+    }, 
+ {
+        text: "SPC-AK-21S啡色",
+        img: "./img/窗帘/SPC-AK-21S啡色.jpg",
+        name: "窗帘",
+    }, 
+ {
+        text: "SPC-AK-48S杏色条纹",
+        img: "./img/窗帘/SPC-AK-48S杏色条纹.jpg",
+        name: "窗帘",
+    }, 
         ],
         墙布挂板: [
             {
