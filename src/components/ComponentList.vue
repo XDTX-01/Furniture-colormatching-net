01653657c99db67fa0af621be10a492167e64eff
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2024-10-31 15:28:52 +0800

    修改

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -249,12 +249,12 @@ export default {
           },
              {
             text: "意大利A682灰皮",
-            img: "./img/皮/意大利皮/22意大利A682灰皮.JPG",
+            img: "./img/皮/意大利皮/22意大利A682灰皮.jpg",
             name: "皮",
              },
             {
             text: "意大利A683啡皮",
-            img: "./img/皮/意大利皮/23意大利A683啡皮.JPG",
+            img: "./img/皮/意大利皮/23意大利A683啡皮.jpg",
             name: "皮",
             },
           {
@@ -692,6 +692,36 @@ export default {
         img: "./img/窗帘/SPC-AK-48S杏色条纹.jpg",
         name: "窗帘",
     }, 
+            {
+        text: "30HC033-1",
+        img: "./img/窗帘/30HC033-1.jpg",
+        name: "窗帘",
+    },
+            {
+        text: "30HC033-2",
+        img: "./img/窗帘/30HC033-2.jpg",
+        name: "窗帘",
+    },
+            {
+        text: "30HC033-3",
+        img: "./img/窗帘/30HC033-3.jpg",
+        name: "窗帘",
+    },
+            {
+        text: "30HC033-4",
+        img: "./img/窗帘/30HC033-4.jpg",
+        name: "窗帘",
+    },
+            {
+        text: "30HC033-5",
+        img: "./img/窗帘/30HC033-5.jpg",
+        name: "窗帘",
+    },
+            {
+        text: "30HC033-6",
+        img: "./img/窗帘/30HC033-6.jpg",
+        name: "窗帘",
+    },    
         ],
         墙布挂板: [
             {
