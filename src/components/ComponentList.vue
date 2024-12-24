9a918bb8234063e2eedfadabcacc315ee14924d7
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2024-12-24 16:14:46 +0800

    更新 ComponentList.vue

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -802,7 +802,7 @@ export default {
             name: "墙布挂板",
           },
          {
-            text: "CS-823",
+            text: "BW-20",
             img: "./img/墙布挂板/CS-823.jpg",
             name: "墙布挂板",
           },
