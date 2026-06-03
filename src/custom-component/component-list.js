6baf3a1e99a11018e0f30ffb34b7344a83d66f56
Andy Deng <112189072+XDTX-01@users.noreply.github.com>
2026-06-03 15:53:38 +0800

    Update component-list.js

--- src/custom-component/component-list.js
+++ src/custom-component/component-list.js
@@ -500,7 +500,7 @@ let newList = [
         name: "布",
     },
     {
-        text: "R806-0730",
+        text: "R806-07",
         img: "./img/布/31-R806-0730-tuya.jpg",
         name: "布",
     },
